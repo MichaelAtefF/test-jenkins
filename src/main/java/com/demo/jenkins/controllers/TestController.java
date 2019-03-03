72662bab216979a553f3fb463c23175d0abd6878
Michael Fouad <michael.atef@radius60.com>
2019-03-03 17:36:17 +0200

    init commit

---
+++ src/main/java/com/demo/jenkins/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.demo.jenkins.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public ResponseEntity<Object> test(){
+        return ResponseEntity.ok().body(null);
+    }
+}
